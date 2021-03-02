63442315a96c14cc753c2c078e70583ea7404744
xiaobu <cnxobo@gmail.com>
2021-03-02 22:48:34 +0800

    add some comments

--- src/main/java/org/xobo/dorado/exposedservice/security/service/DoradoExposedServiceViewParserService.java
+++ src/main/java/org/xobo/dorado/exposedservice/security/service/DoradoExposedServiceViewParserService.java
@@ -3,6 +3,11 @@ package org.xobo.dorado.exposedservice.security.service;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * dorado view 解析工具
+ * @author xobo
+ *
+ */
 public interface DoradoExposedServiceViewParserService {
 
   Map<String, Collection<String>> loadServiceUrlMapping();
--- src/main/java/org/xobo/dorado/exposedservice/security/service/DoradoExposedServiceWhiteListProvider.java
+++ src/main/java/org/xobo/dorado/exposedservice/security/service/DoradoExposedServiceWhiteListProvider.java
@@ -2,6 +2,11 @@ package org.xobo.dorado.exposedservice.security.service;
 
 import java.util.Collection;
 
+/**
+ * 白名单提供者
+ * @author xobo
+ *
+ */
 public interface DoradoExposedServiceWhiteListProvider {
   Collection<String> getWhiteList();
 }
